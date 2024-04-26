54864b1fd984c3d89a9f56e529bc83331d6315a4
Puneethkumar <puneethkumarg96@gmail.com>
2024-04-26 19:59:00 +0530

    firebase

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const authRouter = express.Router();
-
-authRouter.route("/signUp").post(async (req, res) => {
-    const {email,password} = req.body;
-    
-});
